03f2a6f6a069c0abe6adf316455cd491914f5c7d
korezonzi <korezonzip@gmail.com>
2020-08-10 10:19:39 +0900

    add show allCategory in controller

---
+++ app/controllers/Category.scala
@@ -0,0 +1,46 @@
+package controllers
+import javax.inject._
+import scala.concurrent.Future
+import play.filters.csrf.CSRFCheck
+import play.filters.csrf.CSRFAddToken
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation.Valid
+import play.api.data.validation.Constraint
+import play.api.data.validation.Invalid
+import play.api.i18n.I18nSupport
+import lib.model.Category
+import lib.persistence.default._
+//import model.common.ViewValueMessage
+import model.common.component.ViewValueCategory
+import model.site.category._
+import scala.languageFeature.postfixOps
+class CategoryController @Inject()(
+  val controllerComponents: ControllerComponents,
+  val checkToken:           CSRFCheck
+) extends BaseController with I18nSupport {
+  implicit val ec = defaultExecutionContext
+
+//  //CSRFトークンをチェックするハンドラー
+//  object CSRFErrorHandler extends ErrorHandler {
+//    def handle(request: RequestHeader, msg: String) = {
+//      Future.successful(Redirect(routes.TodoController.showAllTodo))
+//    }
+//  }
+
+  def showAllCategory() = Action.async { implicit request =>
+    for {
+      categorySeq <- CategoryRepository.getAll
+    } yield {
+      val vv = ViewValueCategoryList(
+        title       = "カテゴリー一覧",
+        categorySeq = categorySeq.map(ViewValueCategory.create(_)),
+        cssSrc      = Seq("main.css", "category.css"),
+        jsSrc       = Seq("main.js")
+      )
+      Ok(views.html.site.category.List(vv))
+    }
+  }
+}
